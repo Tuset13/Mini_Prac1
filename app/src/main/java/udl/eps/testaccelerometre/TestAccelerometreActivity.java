ec8760b049ff4752517e324f48a3771e63b0aacb
Tuset13 <37141132+Tuset13@users.noreply.github.com>
2020-02-19 17:19:45 +0100

    Sensors introduits al Android Manifest. ListView configurat. Reconeixement dels sensors per tipus, i actualització de canvis.

--- app/src/main/java/udl/eps/testaccelerometre/TestAccelerometreActivity.java
+++ app/src/main/java/udl/eps/testaccelerometre/TestAccelerometreActivity.java
@@ -8,6 +8,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,9 +16,9 @@ public class TestAccelerometreActivity extends Activity implements SensorEventLi
     private SensorManager sensorManager;
     private SensorManager sensorManagerLlum;
     private boolean color = false;
-    private TextView view;
+    private TextView viewAcc;
     private TextView viewText;
-    private TextView viewLlum;
+    private ListView viewLlum;
     private long lastUpdate;
     private long lastUpdateLlum;
 
@@ -28,31 +29,54 @@ public class TestAccelerometreActivity extends Activity implements SensorEventLi
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        view = findViewById(R.id.textView);
-        view.setBackgroundColor(Color.GREEN);
+        viewAcc = findViewById(R.id.textView);
+        viewAcc.setBackgroundColor(Color.GREEN);
         viewText = findViewById(R.id.textView2);
-        viewLlum = findViewById(R.id.textView3);
+        viewLlum = findViewById(R.id.listView3);
         viewLlum.setBackgroundColor(Color.YELLOW);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         if (sensorManager != null) {
-            sensorManager.registerListener(this,
-                    sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                    SensorManager.SENSOR_DELAY_NORMAL);
-            // register this class as a listener for the accelerometer sensor
+
+            Sensor sensorAcc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            Sensor sensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+            if(sensorAcc != null)
+            {
+                sensorManager.registerListener(this, sensorAcc, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+            else
+            {
+                viewAcc.setText(R.string.no_acce);
+            }
+
+            if(sensorLight != null)
+            {
+                sensorManager.registerListener(this, sensorLight, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+            else
+            {
+                viewLlum.setTextAlignment(R.string.no_light);
+            }
 
             lastUpdate = System.currentTimeMillis();
-        }
-        else
-        {
-            view.setText(R.string.no_acce);
+            lastUpdateLlum = System.currentTimeMillis();
         }
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        getAccelerometer(event);
+
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        {
+            getAccelerometer(event);
+        }
+
+        if(event.sensor.getType() == Sensor.TYPE_LIGHT)
+        {
+            getLight(event);
+        }
     }
 
     private void getAccelerometer(SensorEvent event) {
@@ -74,13 +98,17 @@ public class TestAccelerometreActivity extends Activity implements SensorEventLi
 
             Toast.makeText(this, R.string.shuffed, Toast.LENGTH_SHORT).show();
             if (color) {
-                view.setBackgroundColor(Color.GREEN);
+                viewAcc.setBackgroundColor(Color.GREEN);
 
             } else {
-                view.setBackgroundColor(Color.RED);
+                viewAcc.setBackgroundColor(Color.RED);
             }
             color = !color;
         }
+    }
+
+    private void getLight(SensorEvent event) {
+
     }
 
     @Override
